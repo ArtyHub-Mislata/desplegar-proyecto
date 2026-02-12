2e829956b0674a58eae1abcd8962e3dce79ecc5f
Luis Miguel Olmeda Romero <olmedaromeroluismiguel@gmail.com>
2026-02-12 12:35:09 +0100

    Update Docker commands in up.sh script

--- up.sh
+++ up.sh
@@ -2,6 +2,5 @@
 
 read -p "Github password... " passwd
 echo -e $passwd >> /home/alumno/artyhub/passwd
-docker compose down
-docker compose build --no-cache
-docker compose up -d
+docker compose pull
+docker compose up -d --build
